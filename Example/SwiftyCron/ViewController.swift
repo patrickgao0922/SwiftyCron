140da22de753e63d409e6995b1263a789199ca57
Patrick Gao <patrickgao0922@gmail.com>
2018-01-13 16:16:44 +1100

    init dev

--- Example/SwiftyCron/ViewController.swift
+++ Example/SwiftyCron/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import SwiftyCron
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let cronString = "39 11 02 08 *"
+        let swiftyCron = SwiftyCron(cronString:cronString)
+        
+        print(swiftyCron?.day)
     }
 
     override func didReceiveMemoryWarning() {
--- SwiftyCron/Classes/SwiftyCron.swift
+++ SwiftyCron/Classes/SwiftyCron.swift
@@ -20,7 +20,7 @@ public class SwiftyCron {
     /// - invalidMonth: month should be in the range of 1 - 12 or *
     /// - invalidWeekday: weekday should be in the range of 1 - 7 or *
     /// - invalidYear: minitue should be in the range of 1900 - 3000 or *
-    enum DecomposingError:Error {
+    public enum DecomposingError:Error {
         case invalidCronExpression
         case invalidMinute
         case invalidHour
@@ -39,7 +39,7 @@ public class SwiftyCron {
         case year
     }
     
-    enum Weekday:Int {
+    public enum Weekday:Int {
         
         case monday = 1
         case tuesday
@@ -50,7 +50,7 @@ public class SwiftyCron {
         case sunday
     }
     
-    enum Month:Int {
+    public enum Month:Int {
         case january = 1
         case feburary
         case march
@@ -65,24 +65,39 @@ public class SwiftyCron {
         case december
     }
     
-    var cronString:String?
-    var minute:Int?
-    var hour:Int?
-    var day:Int?
-    var month:Month?
-    var weekday:Weekday?
-    var year:Int?
+    public var cronString:String?
+    public var minute:Int?
+    public var hour:Int?
+    public var day:Int?
+    public var month:Month?
+    public var weekday:Weekday?
+    public var year:Int?
     
     
     /// CronParser initializer with cronString
     ///
     /// - Parameter cronString: Cron expression String
-    init(cronString:String) {
-        try? decompose(cronString: cronString)
+    public init?(cronString:String) {
+        do {
+            try decompose(cronString: cronString)
+        } catch {
+            return nil
+        }
     }
     
-    init(minute:Int? = nil, hour:Int? = nil, day:Int? = nil, month:Month? = nil, weekday:Weekday? = nil, year:Int? = nil) {
-        validate(minute: minute, hour: hour, day: day, month: month, weekday: weekday, year: year)
+    public init?(minute:Int? = nil, hour:Int? = nil, day:Int? = nil, month:Month? = nil, weekday:Weekday? = nil, year:Int? = nil) {
+        do {
+            try validate(minute: minute, hour: hour, day: day, month: month, weekday: weekday, year: year)
+            self.minute = minute
+            self.hour = hour
+            self.day = day
+            self.month = month
+            self.weekday = weekday
+            self.year = year
+        } catch {
+            return nil
+        }
+        
     }
     
     /// Decompose cronString to each cron components and assign them into each properties
@@ -95,9 +110,10 @@ public class SwiftyCron {
         self.minute = Int(cronExpressionArray[CronCompnent.minute.rawValue])
         self.hour = Int(cronExpressionArray[CronCompnent.hour.rawValue])
         self.day = Int(cronExpressionArray[CronCompnent.day.rawValue])
-        self.month = Month(rawValue: Int(cronExpressionArray[CronCompnent.month.rawValue])!)
-        self.weekday = Weekday(rawValue: Int(cronExpressionArray[CronCompnent.weekday.rawValue])!)
-        self.year = Int(cronExpressionArray[CronCompnent.year.rawValue])
+        self.month = Int(cronExpressionArray[CronCompnent.month.rawValue]) == nil ? nil : Month(rawValue: Int(cronExpressionArray[CronCompnent.month.rawValue])!)
+        self.weekday =  Int(cronExpressionArray[CronCompnent.weekday.rawValue]) == nil ? nil : Weekday(rawValue: Int(cronExpressionArray[CronCompnent.weekday.rawValue])!)
+        
+        self.year = cronExpressionArray.count == CronCompnent.year.rawValue + 1 ? Int(cronExpressionArray[CronCompnent.year.rawValue]) :  nil
         
     }
     
@@ -196,18 +212,18 @@ public class SwiftyCron {
         }
         
         //        Validate month
-        if month != nil {
-            guard (month! >= 1 && day! <= 12) else {
-                throw DecomposingError.invalidMonth
-            }
-        }
-        
-        //        Validate weekday
-        if weekday != nil {
-            guard (weekday! >= 1 && weekday! <= 7) else {
-                throw DecomposingError.invalidWeekday
-            }
-        }
+//        if month != nil {
+//            guard (month! >= 1 && month! <= 12) else {
+//                throw DecomposingError.invalidMonth
+//            }
+//        }
+//        
+//        //        Validate weekday
+//        if weekday != nil {
+//            guard (weekday! >= 1 && weekday! <= 7) else {
+//                throw DecomposingError.invalidWeekday
+//            }
+//        }
         
         //        Validate year
         if year != nil {
